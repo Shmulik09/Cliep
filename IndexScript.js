f97cee49eb63bddbbb01a6d0808ed70a837b6373
Shmulik09 <shmolikfernando@gmail.com>
2024-05-29 09:27:26 +0300

    Changing the website from watching videos to building city

--- IndexScript.js
+++ IndexScript.js
@@ -1,12 +1,47 @@
-var JSTopButtons = document.getElementsByClassName("TopButtons"); // Defiens TopButtons JS variable, it refers to the buutons in the top of the screen
+const CitySlots = [];
+const divs = [];
 
-for (var i = 0; i < JSTopButtons.length; i++) {
-  JSTopButtons[i].addEventListener("mouseover", function () {
-    this.style.backgroundColor = "#B0EBB4";
-  });
+// Creates 50 div elements and adds them to the FullMap container
+for (var i = 0; i < 50; i++) {
+  CitySlots.push("Slot" + i);
+  const div = document.createElement('div');
+  div.classList.add('Slot' + i);
+  div.classList.add('BuildingSlots');
+  div.classList.add('BuildingSlotsHover');
+  divs.push(div);
+  div.innerHTML = "Open Site";
+  div.style.border = "1px solid black";
+  document.getElementsByClassName("FullMap")[0].appendChild(div);
+}
 
-  JSTopButtons[i].addEventListener("mouseout", function () {
-    this.style.backgroundColor = "#ACE1AF";
-    this.style.color = "black";
+// Creates shortcut to access BuildingSlots divs
+const JSSlotsDivs = document.getElementsByClassName("BuildingSlots");
+
+for (let j = 0; j < JSSlotsDivs.length; j++) {
+  JSSlotsDivs[j].addEventListener("click", function() {
+    // Check if the clicked div is already marked
+    if (this.classList.contains("Marked")) {
+      // If already marked, unmark it
+      this.classList.remove('Marked');
+    } else {
+      // If not marked, unmark any previously marked div
+      for (let k = 0; k < JSSlotsDivs.length; k++) {
+        if (JSSlotsDivs[k].classList.contains("Marked")) {
+          JSSlotsDivs[k].classList.remove('Marked');
+        }
+      }
+      // Mark the newly clicked div
+      this.classList.add('Marked');
+      this.classList.remove('BuildingSlotsHover');
+    }
   });
 }
+
+// End of the building slots code
+
+
+// Start of the top bars code
+
+// Left bar code
+// Will include animations and menu button
+
