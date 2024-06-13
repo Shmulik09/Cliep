fab630ea3a2d521fb6f283344accef289fce39fc
Shmulik09 <shmolikfernando@gmail.com>
2024-06-13 08:35:40 +0300

    .

--- IndexScript.js
+++ IndexScript.js
@@ -1,11 +1,11 @@
 const CitySlots = [];
-const divs = [];
 let IsPuased = false;
 let PopulationFromNewBuildings;
-let JSPopulationNumber = parseInt(
-  document.getElementById("PopulationNumber").innerHTML
-);
+let JSPopulationNumber = parseInt(document.getElementById("PopulationNumber").innerHTML) || 0;
+let JSMoneyNumber = parseInt(document.getElementById("MoneyNumber").innerHTML) || 0;
+var MoneyLength = document.getElementById("MoneyNumber").innerHTML.length;
 const JSBuildingsBar =  document.getElementsByClassName('BuildingsBar');
+
 
 addEventListener("keydown", (event) => {
   if (event.isComposing || event.keyCode == 32) {
@@ -13,9 +13,11 @@ addEventListener("keydown", (event) => {
       document.getElementById("BodyId").style.background = color =
         "rgba(218, 67, 30, 0.719)";
       IsPuased = true;
+      document.getElementById('BTitle').style.textShadow = "1px 2px green";
     } else {
       document.getElementById("BodyId").style.backgroundColor = "#97BE5A";
       IsPuased = false;
+      document.getElementById('BTitle').style.textShadow = "1px 1px red";
     }
   }
 });
@@ -25,16 +27,15 @@ for (var i = 0; i < 50; i++) {
   CitySlots.push("Slot" + i);
   const div = document.createElement("div");
   div.classList.add("Slot" + i);
-  div.classList.add("BuildingSlots");
-  div.classList.add("BuildingSlotsHover");
-  divs.push(div);
+  div.classList.add("CitySlots");
+  div.classList.add("CitySlotsHover");
   div.innerHTML = "Open Site";
   div.style.border = "1px solid black";
   document.getElementsByClassName("FullMap")[0].appendChild(div);
 }
 
-// Creates shortcut to access BuildingSlots divs
-const JSSlotsDivs = document.getElementsByClassName("BuildingSlots");
+// Creates shortcut to access CitySlots divs
+const JSSlotsDivs = document.getElementsByClassName("CitySlots");
 
 for (let j = 0; j < JSSlotsDivs.length; j++) {
   JSSlotsDivs[j].addEventListener("click", function () {
@@ -51,7 +52,7 @@ for (let j = 0; j < JSSlotsDivs.length; j++) {
       }
       // Mark the newly clicked div
       this.classList.add("Marked");
-      this.classList.remove("BuildingSlotsHover");
+      this.classList.remove("CitySlotsHover");
       
       if (!IsPuased) {
         PopulationFromNewBuildings = Math.floor(Math.random() * 10);
@@ -63,17 +64,24 @@ for (let j = 0; j < JSSlotsDivs.length; j++) {
   });
 }
 
-while (JSPopulationNumber > 0) {
-  MoneyFromTaxes = Math.floor(Math.random() * 1);
-}
+setInterval(function() {
+  if (JSPopulationNumber > 0 && !IsPuased) {
+    let MoneyFromTaxes = JSPopulationNumber * 2;
+    console.log(MoneyFromTaxes);
+    MoneyFromTaxes = Math.floor(Math.random() * MoneyFromTaxes);
+    JSMoneyNumber += MoneyFromTaxes;
+    document.getElementById("MoneyNumber").innerHTML = JSMoneyNumber;
+    if (document.getElementById("MoneyNumber").innerHTML.length > MoneyLength)
+      {
+        MoneyLength = document.getElementById("MoneyNumber").innerHTML.length;
+        document.getElementsByClassName('InfoBar').style.width = document.getElementsByClassName('InfoBar').style.width + 5;
+      }
+  }
+}, 300);
 // End of the building slots code
 
 // Start of the top bars code
 
-function BuildingsBarAnimations()
-{
-
-}
 
 
 
